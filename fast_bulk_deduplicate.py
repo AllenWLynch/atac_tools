c001cd9f0cbc64f70d81ac7eda644a98a643fd9b
Allen Lynch <alynch@ds.dfci.harvard.edu>
2020-10-01 15:54:35 -0400

    tested fast_deduplication

--- fast_bulk_deduplicate.py
+++ fast_bulk_deduplicate.py
@@ -31,6 +31,7 @@ class FragmentWriter:
             #if location / barcode combination not yet recorded, add to memory
             if not barcode in self.fragment_memory[location_id]:
                 self.fragment_memory[location_id][barcode] = True
+                #self.fragment_memory[location_id].append(barcode)
         else:
             raise ValueError('Cannot commit new fragment to memory without flushing previous fragments')
     
@@ -44,8 +45,9 @@ class FragmentWriter:
                     print('\t'.join([*location_id, barcode, str(self.dup_groups_index)]))
                 self.dup_groups_index+=1
             else:
-                print('\t'.join([*location_id, barcodes[0], '-1']))
+                print('\t'.join([*location_id, barcodes[0], '-1' if len(barcodes) == 1 else str(len(barcodes))]))
 
+        del self.fragment_memory
         self.fragment_memory = defaultdict(dict)
 
 
@@ -56,18 +58,19 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group(required = True)
     group.add_argument('--mark', action='store_true', default=False, help = 'Mark duplicates in fragment file, but do not remove. If fragment is unique, mark with -1, else mark with an integer denoting its duplicate group membership.')
     group.add_argument('--assign', action = 'store_true', default=False, help = 'Assign a duplicate fragment to a cell.')
-    parser.add_argument('-n','--numdups',action = 'store_true', default=False, help = 'Record the number of duplicates oberserved for each fragment')
+    parser.add_argument('-q','--quiet',action = 'store_true',default=False, help = 'Run quietly')
+
     
     args = parser.parse_args()
 
-    fragment_writer = FragmentWriter(args.mark)    
+    fragment_writer = FragmentWriter(args.mark)
 
     for linenum, fragment in enumerate(args.input.readlines()):
         
         try:
             (chrom,start,end,barcode) = fragment.strip().split('\t')[:4]
         except ValueError:
-            print('ERROR: line #{} has incorrect number of fields. Make sure this file is tab-delineated.'.format(str(linenum + 1)), file = sys.stderr)
+            print('\nERROR: line #{} has incorrect number of fields. Make sure this file is tab-delineated.'.format(str(linenum + 1)), file = sys.stderr)
             exit(1)
         
         try:
@@ -78,9 +81,13 @@ if __name__ == "__main__":
             fragment_writer.flush_memory()
             fragment_writer.add_fragment(chrom, start, end, barcode)
         except NotSortedError:
-            print('ERROR: line #{} is out of order. This will give an incorrect deduplication. Use cmd "sort -k1,1 -k2,2n [fragment_file]" before bulk deduplication.'.format(str(linenum+1)),
+            print('\nERROR: line #{} is out of order. This will give an incorrect deduplication. Use cmd "sort -k1,1 -k2,2n [fragment_file]" before bulk deduplication.'.format(str(linenum+1)),
                 file = sys.stderr)
             exit(1)
 
+        if not args.quiet and linenum % 1000 == 0:
+            print('\rProcessed {:.2f} million fragments ...'.format(linenum / 1000000), end = '', file = sys.stderr)
+
     fragment_writer.flush_memory()
+    print('',file = sys.stderr)
     
