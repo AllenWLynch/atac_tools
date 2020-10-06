3a5a9249e161273a91976fa588a34e00b1d6fcde
Allen Lynch <alynch@ds.dfci.harvard.edu>
2020-10-06 09:03:06 -0400

    added xgboost modelilng

--- train_xgbt_model.py
+++ train_xgbt_model.py
@@ -3,15 +3,18 @@ import sklearn
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from xgboost import XGBRegressor
 import numpy as np
 from sklearn.metrics import accuracy_score
 from joblib import dump
+import json
+from sklearn.metrics import mean_squared_error
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l','--labels',type = argparse.FileType('r'), required= True)
-    parser.add_argument('-w','--weights', type = argparse.FileType('r'), required=True)
+    parser.add_argument('-t','--target',type = argparse.FileType('r'), required= True)
+    parser.add_argument('-w','--weights', type = argparse.FileType('r'))
     parser.add_argument('-m','--matrix',type = str, required=True)
     parser.add_argument('-o','--out_model', type = str, required = True)
     parser.add_argument('-c','--cores',type = int, required= True)
@@ -21,21 +24,28 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    #test to make sure dump will work to specified file
+    dump({}, args.out_model)
+
     assert(args.cores > 0)
 
     print('Loading data ...')
 
-    labels = np.array([
-        int(label.strip()) for label in args.labels.readlines()
+    targets = np.array([
+        float(target.strip()) for target in args.target.readlines()
     ])
 
-    weights = np.array([
-        float(weight.strip()) for weight in args.weights.readlines()
-    ])
+    if args.weights:
+        weights = np.array([
+            float(weight.strip()) for weight in args.weights.readlines()
+        ])
+    else:
+        weights = np.ones(len(targets))
 
     features = np.load(args.matrix)
 
-    X_train, X_test, weights_train, weights_test, y_train, y_test = train_test_split(features, weights, labels, test_size = args.test_size, random_state = 1234)
+    X_train, X_test, y_train, y_test, weights_train, weights_test = \
+        train_test_split(features, weights, targets, test_size = args.test_size, random_state = 1234)
 
     if args.num_training_samples > 0:
         X_train, weights_train, y_train = X_train[:args.num_training_samples], weights_train[:args.num_training_samples], y_train[:args.num_training_samples]
@@ -47,33 +57,22 @@ if __name__ == "__main__":
 
     print('Training model ...')
 
-    param_grid = {
-        'C' : [0.01,0.1,1.0,10.0,100.0]
-    }
-    svm_grid = GridSearchCV(
-        LinearSVC(dual = False), 
-            param_grid = param_grid, n_jobs = args.cores, scoring = 'roc_auc', cv = 5)\
+    param_grid = json.loads(args.param_config.read())
+
+    trees_grid = GridSearchCV(XGBRegressor(n_jobs = 1), 
+            param_grid = param_grid, n_jobs = args.cores, scoring = 'neg_mean_squared_error', cv = 10)\
                 .fit(X_train, y_train, sample_weight = weights_train)
 
-    model = svm_grid.best_estimator_
+    model = trees_grid.best_estimator_
 
-    print('Best AUC achieved: {}'.format(str(svm_grid.best_score_)))
+    print('Best MSE achieved: {}'.format(str(-1 * trees_grid.best_score_)))
 
     prediction = model.predict(X_test)
-    accuracy = accuracy_score(y_test, prediction, sample_weight = weights_test)
+    score = mean_squared_error(y_test, prediction, sample_weight = weights_test)
 
-    print('Validation set accuracy: {}'.format(str(accuracy)))
+    print('Validation set MSE: {}'.format(str(score)))
     print('Saving model ...')
-    saved = False
-    filepath = args.out_model
-    while not saved:
-        try:
-            dump(model, filepath)
-            saved = True
-        except Exception as err:
-            print(err)
-            filepath = input('Alternative save file (enter to exit):')
-            if filepath == '':
-                raise err
-
+    
+    dump(model, args.out_model)
+        
     print('Done!')
